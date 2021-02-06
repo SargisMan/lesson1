0ba3589b7b91fba5f0e279f5ba190c873b4daad0
SargisMan <sargis.mansuryan@gmail.com>
2021-02-06 17:50:58 +0400

    lesson2_s1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Default, {obj, obj2} from './test.js/test';
+import Name from './Components/name';
+import SurName from './Components/surename';
+import Age1 from './Components/age';
+import City from './Components/city';
+import Street from './Components/street'
+
 
 function App() {
+  console.log(obj);
+  console.log(obj2);
+  console.log('Default',Default);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Name />
+      <SurName />
+      <Age1 />
+      <City />
+      <Street />
+      {/* <Test />
+      <Info /> */}
+          </div>
   );
+
+// function Test () {
+//   return <p>Hello world</p>
+// }
+// function Info () {
+//   return <div className='info'>
+//     <span>Name:</span>
+//     <span>LastName:</span>
+//     <span>Age:</span>
+//   </div>
+// }
+
 }
 
 export default App;
